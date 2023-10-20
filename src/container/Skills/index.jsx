d9b4e2044055ab8b8d332bc5c3e3cf741c6613de
Bhaskar Dey <bhaskar@logic-square.com>
2023-10-20 19:03:47 +0530

    second commit

--- src/container/Skills/index.jsx
+++ src/container/Skills/index.jsx
@@ -1,13 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import "./index.scss"
 import { motion } from "framer-motion"
 import { client, urlFor } from "../../client"
 import AppWrapper from "../../wrapper/AppWrapper"
-import ReactTooltip from "react-tooltip"
-
+import { Tooltip as ReactTooltip } from 'react-tooltip'
 const Skills = () => {
+  const [experiences, setExperiences] = useState([])
+  const [skills, setSkills] = useState([])
+
+
+  useEffect(() => {
+    const query = '*[_type=="experiences"]'
+    const skillsQuery = '*[_type=="skills"]'
+
+
+    client.fetch(query).then((data) => {
+      setExperiences(data)
+    })
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data)
+    })
+
+  }, [])
+
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='head-text'>Skills & Experience</h2>
+
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list'>
+
+        </motion.div>
+      </div>
+    </>
   )
 }
 
